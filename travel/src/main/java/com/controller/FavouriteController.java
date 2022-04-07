2b5ed0c504b48f97bfe6b8a2d365134d96dfddbd
asdfghj249 <271765036@qq.com>
2022-04-07 20:48:47 +0800

    5

--- travel/src/main/java/com/controller/FavouriteController.java
+++ travel/src/main/java/com/controller/FavouriteController.java
@@ -81,7 +81,7 @@ public class FavouriteController {
 
     @RequestMapping("/favouriteRank")
     public String favouriteRank(Model model,Page page){
-        //测试合并分支2223334
+        //测试合并分支22233345
         page.setCount(6);
         PageHelper.offsetPage(page.getStart(),page.getCount());
         List<Route> routes = favouriteService.FavRank();
